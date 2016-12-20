fe92064f35e56cf62c9bd9cb203eb05ddf181482
mbordoni84 <mbordoni@salesforce.com>
2016-12-20 12:36:34 +0100

    Delete config_1.php

--- FuelSDK-PHP-Dot9/config_1.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-    'appsignature' => 'none', 
-	'clientid' => 'j5qjlwnt8y03tpphmsfmycg4',
-	'clientsecret' => 'O3xtoQAsXrzpKkGCsWYnE5kJ',
-	'defaultwsdl' => 'https://webservice.exacttarget.com/etframework.wsdl',
-    'xmlloc' => 'https://webservice.exacttarget.com/etframework.wsdl',
-);
